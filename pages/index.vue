8066cc794646f4a28c3547383af2ee22281f7b74
simultsop <mentorgashi@live.com>
2022-10-15 08:49:22 +0200

    refactor: remove debugging lines, improve proper image listing

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,13 @@
 <template>
-    <img loading="lazy" :src="dog" title="Random" style="width:150px;height:auto" />
-    <button class="px-5 py-2 border rounded hover:bg-teal-200">Test</button>
+    <ul role="list" class="grid grid-cols-2 gap-x-4 gap-y-8 sm:grid-cols-3 sm:gap-x-6 lg:grid-cols-4 xl:gap-x-8">
+        <li v-for="dog in randomDogs" :key="dog" class="relative">
+            <div
+                class="group aspect-w-10 aspect-h-7 block w-full overflow-hidden rounded-lg bg-gray-100 focus-within:ring-2 focus-within:ring-indigo-500 focus-within:ring-offset-2 focus-within:ring-offset-gray-100">
+                <img :src="dog" loading="lazy" alt="" class="pointer-events-none object-cover group-hover:opacity-75" />
+            </div>
+        </li>
+    </ul>
 </template>
 <script setup>
-const dog = await randomDogApi()
+const randomDogs = await randomDogApi(12)
 </script>
